172c5969dc253252aa8bd5070d5d0a1a78a542f0
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2024-10-18 10:07:53 -0700

    data done here

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,17 @@ import "./App.css";
 import Footer from "./pages/fragments/Footer";
 import Header from "./pages/fragments/Header";
 import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function App({ children }) {
+  useEffect(() => {
+    const changeLogo = () => {
+      document.title = "Bullbearexperts";
+    };
+
+    changeLogo();
+  }, []);
+
   return (
     <>
       <Header />
