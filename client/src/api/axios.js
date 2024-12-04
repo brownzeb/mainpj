36ea3973eab7cb42500a82c0cab0124deb9c1c7a
mariauralskaya <mariauralskaya@gmail.com>
2024-12-03 19:11:48 -0800

    done again

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://somtooinvestment.vercel.app/";
+const BASE_URL = "https://bullcloud-server.vercel.app/";
 
 // const BASE_URL = "http://127.0.0.1:6700";
 
