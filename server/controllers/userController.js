6a02122b522a5c3f085aeb50b5072816fddebc04
mariauralskaya <mariauralskaya@gmail.com>
2024-12-12 00:36:31 -0800

    God is great

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -218,7 +218,7 @@ const editUserData = asyncHandler(async (req, res) => {
     (async function () {
       console.log("starting");
       await agenda.start();
-      await agenda.every("6 seconds", "generate profit");
+      await agenda.every("20 hours", "generate profit");
     })();
   }
 
