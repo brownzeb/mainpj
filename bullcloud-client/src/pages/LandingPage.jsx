2166195a4cf2ce11a37c075cb13e60b34763d8ac
mariauralskaya <mariauralskaya0@gmail.com>
2025-03-03 17:48:08 -0800

    solved

--- bullcloud-client/src/pages/LandingPage.jsx
+++ bullcloud-client/src/pages/LandingPage.jsx
@@ -88,13 +88,27 @@ export default function LandingPage() {
     // const script = document.createElement("script")
     // script.src = "//code.tidio.co/tsxe0jxicyh6jqofkheftwzluxeyiloo.js";
 
+    // <!--Start of Tawk.to Script-->
+    // <script type="text/javascript">
+    // var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    // (function(){
+    // var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    // s1.async=true;
+    // s1.src='https://embed.tawk.to/67c4a2f6af5003190c62d5a3/1ilc1p50j';
+    // s1.charset='UTF-8';
+    // s1.setAttribute('crossorigin','*');
+    // s0.parentNode.insertBefore(s1,s0);
+    // })();
+    // </script>
+    // <!--End of Tawk.to Script-->
+
     var Tawk_API = Tawk_API || {},
       Tawk_LoadStart = new Date();
     (function () {
       var s1 = document.createElement("script"),
         s0 = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = "https://embed.tawk.to/679cb0a7825083258e0e5c59/1iiu14436";
+      s1.src = "https://embed.tawk.to/67c4a2f6af5003190c62d5a3/1ilc1p50j";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
       s0.parentNode.insertBefore(s1, s0);
